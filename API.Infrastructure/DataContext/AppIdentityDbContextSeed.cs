03cce8d6110e5c1b4287e9d7a65133e8313a1357
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-12-28 18:41:21 +0300

    Account controller, login işlemleri, database'e otomatik veri ekleme

---
+++ API.Infrastructure/DataContext/AppIdentityDbContextSeed.cs
@@ -0,0 +1,33 @@
+﻿using API.Core.DbModels.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infrastructure.DataContext
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Mehmet",
+                    Email = "mehmet.muharrem@ceylan.com",
+                    UserName = "mehmet.muharrem@ceylan.com",
+                    Address = new Address
+                    {
+                        FirstName = "Mehmet",
+                        LastName = "Ceylan",
+                        Street = "Istanbun Cad",
+                        City = "Istanbul",
+                        State = "TR",
+                        ZipCode = "34000"
+                    }
+                };
+                await userManager.CreateAsync(user, "MyPassword!23");
+            }
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,44 @@
+using API.Core.DbModels.Identity;
+using API.Infrastructure.DataContext;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+        }*/
+
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = services.GetRequiredService<StoreContext>();
+                    await identityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
+                    host.Run();
+                }
+                catch (Exception ex)
+                {
+                    var logger= loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An Error ocurred during migration");
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
