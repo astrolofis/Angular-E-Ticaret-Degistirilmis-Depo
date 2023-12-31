afd0a190b645b799da6a90b5b3c8b7b57abe676c
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2023-12-31 17:36:57 +0300

    Validation işlemleri ve swagger config authentication işlemleri yapıldı

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage ="Şifreniz bir büyük, karakter bir küçük karakter ve bi rakam içermelidir!")]
         public string Password { get; set; }
     }
 }
