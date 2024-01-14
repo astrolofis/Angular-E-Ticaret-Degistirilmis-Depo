fd2048645796c18fdfc65d3974e9512a6f9eb3fc
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2024-01-14 16:01:54 +0300

    Order işlemleri yapıldı

---
+++ API.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using API.Core.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+        Task<int> Complete();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- API.Infrastructure/Migrations/20240112122822_OrderEntityAdded.Designer.cs
+++ API.Infrastructure/Migrations/20240112122822_OrderEntityAdded.Designer.cs
@@ -4,14 +4,16 @@ using API.Infrastructure.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace API.Data.Migrations
+namespace API.Infrastructure.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240112122822_OrderEntityAdded")]
+    partial class OrderEntityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
